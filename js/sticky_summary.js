5029e63c5075a3591c160b7113f91f7111830273
estherpee <estherp@sfu.ca>
2016-11-05 13:49:03 -0700

    attempting to figure out how to make a sticky cart using java script

--- js/sticky_summary.js
+++ js/sticky_summary.js
@@ -1,1 +1,46 @@
 'use strict';
+
+
+// var s = $(".summary");
+// var   ss = "summary-stop";
+// var   fh = $(".footer").height();
+// var	position = s.position();
+
+
+// $(window).scroll(function() {
+//   if( $(s)position.bottom > fh ) {
+//     s.addClass(ss);
+//   } else {
+//     s.removeClass(ss);
+//   }
+// });
+
+
+// Position of fixed element from top of the document
+var fixedElementOffset = $('.summary').offset().top;
+// Position of footer element from top of the document.
+// You can add extra distance from the bottom if needed,
+// must match with the bottom property in CSS
+var footerOffset = $('.footer').offset().top - 36;
+
+var fixedElementHeight = $('.summary').height(); 
+
+// Check every time the user scrolls
+$(window).scroll(function (event) {
+
+    // Y position of the vertical scrollbar
+    var y = $(this).scrollTop();
+
+    if ( y >= fixedElementOffset && ( y + fixedElementHeight ) < footerOffset ) {
+        $('.summary').addClass('fixed');
+        $('.summary').removeClass('bottom');          
+    }
+    else if ( y >= fixedElementOffset && ( y + fixedElementHeight ) >= footerOffset ) {
+        $('.summary').removeClass('fixed');           
+        $('.summary').addClass('bottom');
+    }
+    else {
+        $('.summary').removeClass('fixed bottom');
+    }
+
+ });
