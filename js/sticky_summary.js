de9be176e3d923461b693b0f4e124e9ff8301fe3
estherpee <estherp@sfu.ca>
2016-11-06 13:02:33 -0800

    trying to fix sticky order summary so that it stops when it collapses

--- js/sticky_summary.js
+++ js/sticky_summary.js
@@ -2,21 +2,31 @@
 
 
 var s = $(".summary");
-var   ss = "summary-stop";
-var   fh = $("#global-footer").height();
+var ss = "summary-stop";
+var scw = $(".summary-container").width();
+var fh = $("#global-footer").height();
 var csh = $(".checkout-section").height();
 var cah = $(".cart-section").height();
+var width = $(window).width();
+
+// s.css("width", s.parent().width());
 
 
 
 $(window).scroll(function() {
-  if( $(this).scrollTop() > fh) {
+  if( $(this).scrollTop() > ($(window).height() - (fh + 10))) {
     s.addClass(ss);
     $(".summary-container").css("height", csh);
-    $(".summary-container-cart").css("height", cah);
+   	$(".summary-container-cart").css("height", cah);
+
 
   } else {
     s.removeClass(ss);
-  }
-});
+    // s.css("width", s.parent().width());
 
+
+  }
+
+    console.log($(this).scrollTop());
+  	console.log(fh);
+});
