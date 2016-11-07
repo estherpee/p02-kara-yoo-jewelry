0554896a30de3e502e13e0ff347e51ef94a8301d
estherpee <estherp@sfu.ca>
2016-11-07 02:55:16 -0800

    made p sizing consistant between pages, added feedback on hover for featured images, fixed spacing of featured images, changed margins on checkout-finished

--- js/dropdown_nav.js
+++ js/dropdown_nav.js
@@ -9,29 +9,23 @@ $(document).ready(function(){
 
 	
 	    $(".shop-target").mouseenter(function(){
-	        // $("#shop").css("display", "block");
 	        $("#shop").slideDown("normal");
-	        console.log("yes");
 	    });
 
 	    $("#shop").mouseleave(function(){
-	    	// $("#shop").css("display", "none");
 	    	$("#shop").slideUp("normal");
 	    });
 
 	     $(".dropdown-nav").mouseleave(function(){
-	    	// $("#shop").css("display", "none");
 	    	$("#shop").slideUp("normal");
 	    	$("#about").slideUp("normal");
 	    });
 
 	    $(".about-target").mouseenter(function(){
-	        // $("#shop").css("display", "block");
 	        $("#about").slideDown("normal");
 	    });
 
 	    $("#about").mouseleave(function(){
-	    	// $("#shop").css("display", "none");
 	    	$("#about").slideUp("normal");
 	    });
 
