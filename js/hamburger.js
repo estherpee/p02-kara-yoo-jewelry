a3c68b4153569d1dd481b49544f1007c30c96204
estherpee <estherp@sfu.ca>
2016-11-06 17:39:52 -0800

    testing out hamburger menu on a test page

---
+++ js/hamburger.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+
+
+$(document).ready(function(){
+var $toggle = 0;
+	
+	    $(".fa-bars").click(function(){
+	    	if($toggle == 0){
+	        // $(".mobile-main-nav").css("display", "block");
+	        $(".mobile-main-nav").slideDown("normal");
+	        $toggle = 1;
+	    	} else {
+	    	$(".mobile-main-nav").slideUp("normal");
+	    	$toggle = 0;
+	    	};
+	  	});
+
+});
