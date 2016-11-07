37a2f7a3fd5f9321d92788a68aa520fb4a808f69
estherpee <estherp@sfu.ca>
2016-11-07 11:46:48 -0800

    fixed font location error and fixed order summary fixed scroll

--- js/sticky_summary.js
+++ js/sticky_summary.js
@@ -3,70 +3,49 @@
 
 var s = $(".summary");
 var ss = "summary-stop";
-var scw = $(".summary-container").width();
+var sstacked = "summary-stacked";
+var sc = $(".summary-container");
+var scc = $(".summary-container-cart");
 var fh = $("#global-footer").height();
 var csh = $(".checkout-section").height();
 var cah = $(".cart-section").height();
 var ww = $(window).width();
-var cas = $(".cart-section");
 
 
-// s.css("width", s.parent().width());
-
-// if (ww < 879) {
-//  	$(".summary-stop").css("position", "relative"); 
-//  	$(".summary").css("position", "relative");
-//  	s.css("width", "100%");
-//  	$(".summary-container-cart").css("height", "10px");
-// }
 
 if (ww > 879) {
+	$(".summary-container").css("height", csh);
+   	$(".summary-container-cart").css("height", cah);
+
+   	
 	$(window).scroll(function() {
-	  // if( $(this).scrollTop() > 250) {
-	  if( (s.position().top + s.height()) > ($("#global-footer").position().top)) {
+	  if(($(this).scrollTop() + 560) > ($(document).height() - (fh + 15))) {
+	  // if((s.position().top + s.height()) > ($(document).height() - (fh + 40))) {
+	  // if( (s.position().top + s.height()) > ($("#global-footer").position().top)) {
 
 	    s.addClass(ss);
-	    $(".summary-container").css("height", csh);
-	   	$(".summary-container-cart").css("height", cah);
-
 
 	  } else {
 	    s.removeClass(ss);
-	    // s.css("width", s.parent().width());
 
 
 	  }
-		console.log("summary" + (s.position().top + s.height()));
-	    console.log($(this).scrollTop());
-	  	console.log("footer" + ($("#global-footer").position().top));
+		
+
+		console.log(($(document).scrollTop() + 570));
+		console.log(($(document).height() - (fh + 50)));
+
+
 	});
+	
 } else {
-	$(".summary").css("position", "relative");
-  	s.css("width", "100%");
+		s.addClass(sstacked);
+		scc.addClass("summary-container-cart-stacked");
+		sc.addClass("summary-container-stacked");
+  		
 }
 
+$(window).bind('resize', function() {
+     location.reload();
+});
 
-// if (ww > 879) {
-// 	$(window).scroll(function() {
-// 	  if( $(this).scrollTop() > ($(window).height() - (fh + 15))) {
-// 	  // if( (s.position().top + s.height()) > ($("#global-footer").position().top)) {
-
-// 	    s.addClass(ss);
-// 	    $(".summary-container").css("height", csh);
-// 	   	$(".summary-container-cart").css("height", cah);
-
-
-// 	  } else {
-// 	    s.removeClass(ss);
-// 	    // s.css("width", s.parent().width());
-
-
-// 	  }
-// 		console.log("summary" + (s.position().top + s.height()));
-// 	    console.log($(this).scrollTop());
-// 	  	console.log("footer" + ($("#global-footer").position().top));
-// 	});
-// } else {
-// 	$(".summary").css("position", "relative");
-//   	s.css("width", "100%");
-// }
